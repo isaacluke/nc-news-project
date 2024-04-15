1cbaa341b62abddfcaf334728fac1a3858031404
isaacluke <58997330+isaacluke@users.noreply.github.com>
2024-04-15 12:35:22 +0100

    adds /api which provides all endpoints

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express")
 
+const endpoints = require('./endpoints.json')
 const { getAllTopics } = require("./controllers/topics.controllers")
 
 const app = express()
 
 app.get("/api/topics", getAllTopics)
+app.get("/api", (req,res,next)=>{
+    res.status(200).send({endpoints})
+})
 
 app.all("/*", (req,res,next)=>{
     res.status(404).send({msg: "Path not found"})
