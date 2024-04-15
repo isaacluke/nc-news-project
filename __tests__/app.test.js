f88a42f9a7de20cf6bbc1af8beebbaf21742b6f8
isaacluke <58997330+isaacluke@users.noreply.github.com>
2024-04-15 14:09:10 +0100

    adds happy and sad paths for the article_id endpoint, and updates the endpoints.json

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,48 +1,89 @@
-const request = require("supertest")
-const seed = require("../db/seeds/seed")
-const db = require("../db/connection")
-const app = require('../app')
-const data = require('../db/data/test-data')
-const endpoints = require("../endpoints.json")
+const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const db = require("../db/connection");
+const app = require("../app");
+const data = require("../db/data/test-data");
+const endpoints = require("../endpoints.json");
 
-beforeEach(()=>seed(data))
-afterAll(()=>db.end())
+beforeEach(() => seed(data));
+afterAll(() => db.end());
 
-describe("/api/topics",()=>{
-    test("GET 200: Responds with all topics, each of which with the properties of slug and description",()=>{
-        return request(app)
-        .get("/api/topics")
-        .expect(200)
-        .then(({body})=>{
-            const {topics} = body
-            expect(topics.length).toBe(3)
-            topics.forEach((topic)=>{
-                expect(typeof topic.description).toBe('string')
-                expect(typeof topic.slug).toBe('string')
-            })
-        })
-    })
-})
+describe("/api/topics", () => {
+  test("GET 200: Responds with all topics, each of which with the properties of slug and description", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        const { topics } = body;
+        expect(topics.length).toBe(3);
+        topics.forEach((topic) => {
+          expect(typeof topic.description).toBe("string");
+          expect(typeof topic.slug).toBe("string");
+        });
+      });
+  });
+});
 
-describe("/api",()=>{
-    test("GET 200: Responds with a description of all other endpoints available",()=>{
-        return request(app)
-        .get("/api")
-        .expect(200)
-        .then(({body})=>{
-            expect(body.endpoints).toEqual(endpoints)
-        })
-    })
-})
+describe("/api", () => {
+  test("GET 200: Responds with a description of all other endpoints available", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.endpoints).toEqual(endpoints);
+      });
+  });
+});
 
-describe("/*",()=>{
-    test("ALL 404: Responds with a path not found when an incorrect path is invalid",()=>{
-        return request(app)
-        .get("/invalid")
-        .expect(404)
-        .then(({body})=>{
-            const {msg} = body
-            expect(msg).toBe("Path not found")
-        })
-    })
-})
+describe("/api/articles/:article_id", () => {
+  test("GET 200: Responds with an object with the articles properties", () => {
+    return request(app)
+      .get("/api/articles/6")
+      .expect(200)
+      .then(({ body }) => {
+        const { article } = body;
+        const testArticle = {
+          article_id: 6,
+          title: "A",
+          topic: "mitch",
+          author: "icellusedkars",
+          body: "Delicious tin of cat food",
+          created_at: "2020-10-18T01:00:00.000Z",
+          votes: 0,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        };
+        expect(article).toMatchObject(testArticle);
+      });
+  });
+  test("GET 400: Invalid article_id type received", () => {
+    return request(app)
+      .get("/api/articles/invalid-input")
+      .expect(400)
+      .then(({ body }) => {
+        const { msg } = body;
+        expect(msg).toBe("Bad request");
+      });
+  });
+  test("GET 404: article_id not found", () => {
+    return request(app)
+      .get("/api/articles/10000")
+      .expect(404)
+      .then(({ body }) => {
+        const { msg } = body;
+        expect(msg).toBe("Not found")
+      });
+  });
+});
+
+describe("/*", () => {
+  test("ALL 404: Responds with a path not found when an incorrect path is invalid", () => {
+    return request(app)
+      .get("/invalid")
+      .expect(404)
+      .then(({ body }) => {
+        const { msg } = body;
+        expect(msg).toBe("Path not found");
+      });
+  });
+});
---
+++ controllers/articles.controllers.js
@@ -0,0 +1,13 @@
+const { selectArticle } = require("../models/articles.models")
+
+exports.getArticle = (req,res,next) => {
+    const {article_id} = req.params
+    selectArticle(article_id)
+    .then((article)=>{
+        res.status(200).send({article})
+    })
+    .catch((err)=>{
+        console.log(err)
+        next(err)
+    })
+}
