2ff1e58c635402e115430724ca78f4e69bdc90c7
isaacluke <58997330+isaacluke@users.noreply.github.com>
2024-04-17 15:05:19 +0100

    adds sort_by and order queries to get articles endpoint

--- models/articles.models.js
+++ models/articles.models.js
@@ -19,7 +19,9 @@ exports.selectArticle = (article_id) => {
     ON comments.article_id = articles.article_id
     WHERE articles.article_id = $1
     GROUP BY articles.article_id;
-    `, [article_id])
+    `,
+      [article_id]
+    )
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Not found" });
@@ -28,8 +30,24 @@ exports.selectArticle = (article_id) => {
     });
 };
 
-exports.selectAllArticles = (topic) => {
-    let sqlQueryString = `
+exports.selectAllArticles = (topic, sort_by = "created_at", order = "desc") => {
+  const validSortBy = [
+    "article_id",
+    "title",
+    "topic",
+    "author",
+    "created_at",
+    "votes",
+    "article_img_url",
+    "comment_count",
+  ];
+  const validOrders = ["asc", "desc"]
+
+  if(!validSortBy.includes(sort_by) || !validOrders.includes(order)){
+    return Promise.reject({status:400, msg: "Bad request"})
+  }
+
+  let sqlQueryString = `
         SELECT  
             articles.article_id, 
             articles.title,
@@ -41,76 +59,89 @@ exports.selectAllArticles = (topic) => {
             CAST(COUNT(comments.comment_id) AS INT) AS comment_count
         FROM articles
         LEFT JOIN comments
-        ON comments.article_id = articles.article_id
-        `
-    const sqlQueryArray = []
+        ON comments.article_id = articles.article_id 
+        `;
+  const sqlQueryArray = [];
 
-    if (topic){
-        sqlQueryArray.push(topic)
-        sqlQueryString += `WHERE topic = $1`
-    }
+  if (topic) {
+    sqlQueryArray.push(topic);
+    sqlQueryString += `WHERE topic = $1 `;
+  }
 
-    sqlQueryString += `
+  sqlQueryString += `
         GROUP BY articles.article_id
-        ORDER BY created_at DESC;`
+        ORDER BY ${sort_by} ${order} ;`;
 
+  return db.query(sqlQueryString, sqlQueryArray).then(({ rows }) => {
+    return rows;
+  });
+};
 
+exports.selectArticleComments = (article_id) => {
   return db
-    .query(sqlQueryString, sqlQueryArray)
+    .query(
+      `
+    SELECT * 
+    FROM comments
+    WHERE article_id = $1
+    ORDER BY created_at DESC;
+    `,
+      [article_id]
+    )
     .then(({ rows }) => {
       return rows;
     });
 };
 
-exports.selectArticleComments = (article_id) => {
-    return db.query(`
-    SELECT * 
-    FROM comments
-    WHERE article_id = $1
-    ORDER BY created_at DESC;
-    `, [article_id])
-    .then(({rows})=>{
-        return rows
-    })
-}
-
-exports.insertArticleComment = (article_id, {username, body}) => {
-    return db.query(`
+exports.insertArticleComment = (article_id, { username, body }) => {
+  return db
+    .query(
+      `
     INSERT INTO comments
         (body, author, article_id )
     VALUES
         ($1, $2, $3)
     RETURNING *;
-    `, [body, username, article_id])
-    .then(({rows})=>{
-        return rows[0]
-    })
-}
+    `,
+      [body, username, article_id]
+    )
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
 
 exports.updateArticleVotes = (article_id, inc_votes) => {
-    return db.query(`
+  return db
+    .query(
+      `
     UPDATE articles
     SET votes = votes + $1
     WHERE article_id = $2
     RETURNING *;
-    `, [inc_votes, article_id])
-    .then(({rows})=>{
-        if (rows.length === 0) {
-            return Promise.reject({ status: 404, msg: "Not found" });
-          }
-        return rows[0]
-    })
-}
+    `,
+      [inc_votes, article_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not found" });
+      }
+      return rows[0];
+    });
+};
 
 exports.checkArticleExists = (article_id) => {
-    return db.query(`
+  return db
+    .query(
+      `
     SELECT *
     FROM articles
     WHERE article_id = $1;
-    `, [article_id])
-    .then(({rows})=>{
-        if (rows.length === 0){
-            return Promise.reject({ status: 404, msg: "Not found" })
-        }
-    })
-}
+    `,
+      [article_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not found" });
+      }
+    });
+};
