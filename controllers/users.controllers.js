dd362ed0b17a6b9f830d2caada7c8f58eec129d1
isaacluke <58997330+isaacluke@users.noreply.github.com>
2024-04-17 16:20:40 +0100

    adds username endpoint, updates endpoints.json

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,8 +1,17 @@
-const { selectAllUsers } = require("../models/users.models")
+const { selectAllUsers, selectUser } = require("../models/users.models")
 
 exports.getAllUsers = (req,res,next) => {
     selectAllUsers()
     .then((users)=>{
         res.status(200).send({users})
     })
+}
+
+exports.getUser = (req,res,next) => {
+    const {username} = req.params
+    selectUser(username)
+    .then((user)=>{
+        res.status(200).send({user})
+    })
+    .catch(next)
 }
